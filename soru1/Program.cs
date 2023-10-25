45b3e165a0574228c634cada4878aac5c4f165e3
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2023-10-25 23:49:43 +0300

    Ödevi başlattım.

---
+++ soru1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soru1
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+        }
+    }
+}
---
+++ soru2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soru2
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+        }
+    }
+}
