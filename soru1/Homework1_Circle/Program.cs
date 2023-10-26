a1cb59a599d4fcc0c3a0069e8a9fe4541029c0a9
erkutym1 <115299016+erkutym1@users.noreply.github.com>
2023-10-26 19:25:31 +0300

    Add files via upload

---
+++ soru1/Homework1_Circle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_Circle
+{
+    internal class CircleCalcs
+    {
+        static void Main(string[] args)
+        {
+            Double PI = 3.14159;
+
+            Console.Write("input radius: ");
+
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("radius is        : " + radius);
+            Console.WriteLine("diameter is      : " + 2 * radius);
+            Console.WriteLine("circumference is : " + 2 * PI * radius);
+            Console.WriteLine("area is          : " + PI * radius * radius);
+            Console.ReadLine();
+        }
+    }
+
+}
